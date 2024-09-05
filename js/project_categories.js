6c5d2dc2f5ed3985544dc471ffe79efd6eb09413
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2024-09-06 01:52:18 +0500

    Implemented dynamic portolio

---
+++ js/project_categories.js
@@ -0,0 +1,6 @@
+categories = {
+    "web": "Websites",
+    "mobile": "Mobile Apps",
+    "ai": "AI and Machine Learning",
+    "scraping": "Web Scraping and Automation",
+}
---
+++ js/projects.js
@@ -0,0 +1,26 @@
+
+const image_path_prefix = "../img/portfolio/"
+
+projects = [
+    {
+        "title": "Global Pixel Chat",
+        "category": "web",
+        "image_url": image_path_prefix + "global_pixel_chat.png",
+        "demo_link": "https://hammadbinsajjad.github.io/global-pixel-chat/",
+        "is_demo_video": false,
+    },
+    {
+        "title": "Ebay Like Auction Website",
+        "category": "web",
+        "image_url": image_path_prefix + "ebay_like_auction.png",
+        "demo_link": "https://www.youtube.com/watch?v=Pn7pDilGEw4",
+        "is_demo_video": true,
+    },
+    {
+        "title": "Calendar Pro Task App",
+        "category": "mobile",
+        "image_url": image_path_prefix + "calendar_pro.jpg",
+        'demo_link': "",
+        "is_demo_video": false
+    }
+]
